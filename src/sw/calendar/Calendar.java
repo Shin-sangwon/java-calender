43b8096ac3d0b347ce49db82f2f6fae972b43f5c
dnjsml10 <dnjsml10@DESKTOP-QEJ9E35>
2022-05-06 21:15:58 +0900

    beta version

--- src/sw/calendar/Calendar.java
+++ src/sw/calendar/Calendar.java
@@ -1,10 +1,71 @@
 package sw.calendar;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class Calendar {
 	
 	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final String SAVE_FILE = "calendar.dat";
+	
+	private HashMap<Date, PlanItem> planMap;
+	
+	public Calendar() {
+		planMap = new HashMap<>();
+		File f = new File(SAVE_FILE);
+		if(!f.exists()) {
+			System.err.println("no save file");
+			return;
+		}
+		try {
+			Scanner sc = new Scanner(f);
+			while(sc.hasNext()) {
+				String line = sc.nextLine();
+				String words[] = line.split(",");
+				String date = words[0];
+				String detail = words[1].replaceAll("\"", "");
+				PlanItem p = new PlanItem(date, detail);
+				planMap.put(p.getDate(), p);
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void registerPlan(String strDate, String plan) throws ParseException {
+		
+		PlanItem p = new PlanItem(strDate, plan);
+		planMap.put(p.getDate(), p);
+		
+		File f = new File(SAVE_FILE);
+		String item = p.saveString();
+		try {
+			FileWriter fw = new FileWriter(f, true);
+			fw.write(item);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public PlanItem searchPlan(String strDate){
+		Date date = PlanItem.getDatefromString(strDate);
+		return planMap.get(date);
+	}
 	
 	public static boolean isLeapYear(int year) {
 		
@@ -58,7 +119,7 @@ public class Calendar {
 		
 		int flagyear = 1970;
 
-		final int FLAG_WEEKDAY = 3; //1970. 1. 1 Thursday
+		final int FLAG_WEEKDAY = 4; //1970. 1. 1 Thursday
 		
 		int count = 0;
 		
@@ -72,13 +133,14 @@ public class Calendar {
 			count += tmp;				
 		}
 		
-		count += day;
+		count += day -1 ;
 		int weekday = (count + FLAG_WEEKDAY) % 7;
 		return weekday;
 	}
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException{
+		Calendar cal = new Calendar();
+		cal.registerPlan("2017-06-03", "gogo");
 		
 	}
 
--- src/sw/calendar/Prompt.java
+++ src/sw/calendar/Prompt.java
@@ -1,54 +1,132 @@
 package sw.calendar;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
 	
-	public int parseDay(String week) {
-		if(week.equals("SU")) return 0;
-		else if (week.equals("MO")) return 1;
-		else if (week.equals("TU")) return 2;
-		else if (week.equals("WE")) return 3;
-		else if (week.equals("TH")) return 4;
-		else if (week.equals("FR")) return 5;
-		else if (week.equals("SA")) return 1;
-		else return 0;
 	
+	public void printMenu() {
+		
+			System.out.println("+----------------------+");
+			System.out.println("| 1. 일정 등록          ");
+			System.out.println("| 2. 일정 검색           ");
+			System.out.println("| 3. 달력 보기");
+			System.out.println("| h. 도움말 q. 종료");
+			System.out.println("+----------------------+");
+			
 	}
 	
-	public void runPrompt() {	
+	public int parseDay(String week) {
+		switch(week) {
+		case "SU":
+			return 0;
+		case "MO":
+			return 1;
+		case "TU":
+			return 2;
+		case "WE":
+			return 3;
+		case "TH":
+			return 4;
+		case "FR":
+			return 5;
+		case "SA":
+			return 6;
+		default:
+			return 0;		
+		}
+	}
+	
+	public void runPrompt() throws ParseException {	
+		printMenu();
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		int month = -1;
-		int year = -1;
-		
-		while(true) {
-			System.out.println("연도를 입력하세요 (exit : -1)");
-			System.out.print("YEAR> ");
-			year = sc.nextInt();
-			if(year == -1) break;
-			System.out.println("달을 입력하세요");
-			System.out.print("MONTH> ");
-			month = sc.nextInt();
-			
-			
-			
-							
-			if(month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
+	
+		boolean isLoop = true;
+		while (isLoop) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = sc.next();
+			switch(cmd) {
+			case "1":
+				cmdRegister(sc, cal);
+				break;
+			case "2":
+				cmdSearch(sc, cal);
+				break;
+			case "3":
+				cmdCal(sc, cal);
+				break;
+			case "h":
+				printMenu();
+				break;
+			case "q":
+				isLoop = false;
+				break;		
 			}
-			
-			cal.printCalendar(year, month);
-			
 		}
-		
+			
+					
 		System.out.println("bye");
 		sc.close();
-	}
+		}
+		
 	
-	public static void main(String[] args) {
+	
+	private void cmdCal(Scanner sc, Calendar cal) {
+		int month = -1;
+		int year = -1;
+		System.out.println("연도를 입력하세요");
+		System.out.print("YEAR> ");
+		year = sc.nextInt();
+		
+		System.out.println("달을 입력하세요");
+		System.out.print("MONTH> ");
+		month = sc.nextInt();
+		
+		
+		
+						
+		if(month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
+		}
+		
+		cal.printCalendar(year, month);
+		
+	}
+
+	private void cmdSearch(Scanner sc, Calendar cal) {
+		System.out.println("[일정 검색]");
+		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+		String date = sc.next();
+		PlanItem plan;
+		plan = cal.searchPlan(date);
+			
+		if (plan != null) {
+		System.out.println(plan.detail);
+		} else {
+			System.out.println("일정이 없습니다.");
+		}
+	}
+
+	private void cmdRegister(Scanner sc, Calendar cal) throws ParseException {
+        System.out.println("[새 일정 등록]");
+        System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+        String date = sc.next();
+        String text = "";
+        System.out.println("일정을 입력해 주세요.(끝문자=;)");
+		String word;
+		while(!(word = sc.next()).endsWith(";")) {
+			text += word +" ";
+		}
+		word = word.replace(";", "");
+		text += word;
+		cal.registerPlan(date, text);
+    }
+
+	public static void main(String[] args) throws ParseException {
 		
 		Prompt p = new Prompt();
 		p.runPrompt();
